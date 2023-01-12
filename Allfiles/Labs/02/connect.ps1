f9d7c03ec4d99587e64ad9a2ef9d42249e475159
kumud-sharma <33771500+kumud-sharma@users.noreply.github.com>
2023-01-12 11:36:06 +0530

    Create connect.ps1

---
+++ Allfiles/Labs/02/connect.ps1
@@ -0,0 +1,62 @@
+
+$labFilesFolder = 'C:\AllFiles\Labs\03'
+New-Item -ItemType Directory -Path $labFilesFolder
+
+$webClient = New-Object System.Net.WebClient
+$wvdAgentInstallerURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+$wvdAgentInstallerName = 'WVD-Agent.msi'
+$webClient.DownloadFile($wvdAgentInstallerURL,"$labFilesFolder/$wvdAgentInstallerName")
+$wvdBootLoaderInstallerURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+$wvdBootLoaderInstallerName = 'WVD-BootLoader.msi'
+$webClient.DownloadFile($wvdBootLoaderInstallerURL,"$labFilesFolder/$wvdBootLoaderInstallerName")
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-Module -Name PowerShellGet -Force -SkipPublisherCheck
+
+Install-Module -Name Az -AllowClobber -Force
+Install-Module -Name Az.DesktopVirtualization -AllowClobber -Force
+
+$labFilesFolder = 'C:\AllFiles\Labs\03'
+New-Item -ItemType Directory -Path $labFilesFolder
+
+$webClient = New-Object System.Net.WebClient
+$wvdAgentInstallerURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+$wvdAgentInstallerName = 'WVD-Agent.msi'
+$webClient.DownloadFile($wvdAgentInstallerURL,"$labFilesFolder/$wvdAgentInstallerName")
+$wvdBootLoaderInstallerURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+$wvdBootLoaderInstallerName = 'WVD-BootLoader.msi'
+$webClient.DownloadFile($wvdBootLoaderInstallerURL,"$labFilesFolder/$wvdBootLoaderInstallerName")
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-Module -Name PowerShellGet -Force -SkipPublisherCheck
+
+Install-Module -Name Az -AllowClobber -Force
+Install-Module -Name Az.DesktopVirtualization -AllowClobber -Force
+Connect-AzAccount
+
+$resourceGroupName = 'az140-11-RG'
+$hostPoolName = 'az140-24-hp3'
+$registrationInfo = New-AzWvdRegistrationInfo -ResourceGroupName $resourceGroupName -HostPoolName $hostPoolName -ExpirationTime $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+$registrationInfo
+$registrationInfo.Token
+
+Set-Location -Path $labFilesFolder
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $WVDAgentInstallerName", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$($registrationInfo.Token)", "/l* $labFilesFolder\AgentInstall.log" | Wait-Process
+
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $wvdBootLoaderInstallerName", "/quiet", "/qn", "/norestart", "/passive", "/l* $labFilesFolder\BootLoaderInstall.log" | Wait-process
+
+Write-Host Lab Pre-requisite Task Completed Successfully -ForegroundColor Green
+$resourceGroupName = 'az140-11-RG'
+$hostPoolName = 'az140-24-hp3'
+$registrationInfo = New-AzWvdRegistrationInfo -ResourceGroupName $resourceGroupName -HostPoolName $hostPoolName -ExpirationTime $((get-date).ToUniversalTime().AddDays(1).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+$registrationInfo
+$registrationInfo.Token
+
+Set-Location -Path $labFilesFolder
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i $WVDAgentInstallerName", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$($registrationInfo.Token)", "/l* $labFilesFolder\AgentInstall.log" | Wait-Process
+
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $wvdBootLoaderInstallerName", "/quiet", "/qn", "/norestart", "/passive", "/l* $labFilesFolder\BootLoaderInstall.log" | Wait-process
+
+Write-Host Lab Pre-requisite Task Completed Successfully -ForegroundColor Green
